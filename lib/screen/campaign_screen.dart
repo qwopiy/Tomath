2ccef966c20d2dc19dcb1d109a10ac208eb893ee
Bintang Khalik <bintangkhaliqq@gmail.com>
2025-11-21 23:02:30 +0700

    add button widget for campaign_screen.dart

--- lib/screen/campaign_screen.dart
+++ lib/screen/campaign_screen.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:tomath/widget/choice_button.dart';
 
-class CampaignScreen extends StatelessWidget {
+class CampaignScreen extends StatefulWidget {
   const CampaignScreen({super.key});
+
+  @override
+  State<CampaignScreen> createState() => _CampaignScreenState();
+}
+
+class _CampaignScreenState extends State<CampaignScreen> {
+  String questionText = 'Question not loaded';
+  List<String> choices = [
+    'Pilihan 1',
+    'Pilihan 2',
+    'Pilihan 3',
+    'Pilihan 4',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,138 +34,129 @@ class CampaignScreen extends StatelessWidget {
                 // layar atas dibagi 2 juga
                 // atas: soal
                 // bawah: gambar player dan musuh
-                child: Column(
-                  children: [
-                    // atas: soal
-                    Expanded(
-                      // flex: 6,
-                      child: Container(
-                        color: Colors.yellow,
-                        alignment: Alignment.center,
-                        child: Text(
-                          'Soal muncul di sini',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 60),    // padding 100 untuk setting
+                  child: Column(
+                    children: [
+                      // atas: soal
+                      Expanded(
+                        flex: 6,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/ui/kertasPipih.png'),
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          alignment: Alignment.center,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 40, right: 40),
+                            child: Text(
+                              questionText,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    // bawah: gambar player dan musuh
-                    Expanded(
-                      // flex: 4,
-                      child: Container(
-                        color: Colors.orange,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            // gambar player
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.person,
-                                  size: 80,
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  'Player',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600,
+                      // bawah: gambar player dan musuh
+                      Expanded(
+                        flex: 4,
+                        child: Container(
+                          color: Colors.orange,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              // gambar player
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image(image: AssetImage('assets/images/FreakyBug.png'),
+                                    width: 80,
+                                    height: 80,
                                   ),
-                                ),
-                              ],
-                            ),
-                            // gambar musuh
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.person_outline,
-                                  size: 80,
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  'Musuh',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w600,
+                                  SizedBox(height: 8),
+                                  Text(
+                                    'Player',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          ],
+                                ],
+                              ),
+                              // gambar musuh
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image(image: AssetImage('assets/images/FreakyBug.png'),
+                                    width: 80,
+                                    height: 80,
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    'Musuh',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
             // bawah: pilihan jawaban
-            // TODO: betulin bentuk pilihan jawaban
             Expanded(
               flex: 4,
               child: Container(
-                color: Colors.green,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/ui/PapanKayuShort.png'),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                alignment: Alignment.center,
                 // wrap dari kiri ke kanan, overflow ke bawah
                 child: Wrap(
-                  spacing: 20.0,
-                  runSpacing: 20.0,
+                  spacing: 30.0,
+                  runSpacing: 30.0,
                   direction: Axis.horizontal,
                   children: [
-                    ElevatedButton(
+                    ChoiceButton(
+                      buttonText: choices[0],
                       onPressed: () {
                         print('Pilihan 1 ditekan');
                       },
-                      child: Text(
-                        'Pilihan 1',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                        ),
-                      )
                     ),
-                    ElevatedButton(
+                    ChoiceButton(
+                      buttonText: choices[1],
                       onPressed: () {
                         print('Pilihan 2 ditekan');
                       },
-                      child: Text(
-                        'Pilihan 2',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                        ),
-                      )
                     ),
-                    ElevatedButton(
+                    ChoiceButton(
+                      buttonText: choices[2],
                       onPressed: () {
                         print('Pilihan 3 ditekan');
                       },
-                      child: Text(
-                        'Pilihan 3',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                        ),
-                      )
                     ),
-                    ElevatedButton(
+                    ChoiceButton(
+                      buttonText: choices[3],
                       onPressed: () {
                         print('Pilihan 4 ditekan');
                       },
-                      child: Text(
-                        'Pilihan 4',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                        ),
-                      )
                     ),
                   ],
                 ),
