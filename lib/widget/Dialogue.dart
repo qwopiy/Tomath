6903e009df9102cc118974716915327ef4fd0a32
AriiqAlhafizh <ariiqalhafizh69@gmail.com>
2025-12-16 13:11:37 +0700

    add Dialogue features (not finished)

---
+++ lib/widget/Dialogue.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:tomath/widget/woodbutton.dart';
+
+import '../service/app_state_provider.dart';
+
+class Dialogue extends StatefulWidget {
+  final VoidCallback onTap;
+  final String textContent;
+  final String buttonText;
+
+  const Dialogue({
+    super.key,
+    required this.buttonText,
+    required this.textContent,
+    required this.onTap,
+  });
+
+  @override
+  State<Dialogue> createState() => _DialogueState();
+}
+
+class _DialogueState extends State<Dialogue> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppStateProvider>(
+        builder: (context, appState, child) {
+          final size = MediaQuery.of(context).size;
+
+          final panelHeight = size.height * 0.30;
+
+          final topMargin = size.height * 0.55;
+
+          return Container(
+            height: panelHeight,
+            margin: EdgeInsets.only(top: topMargin),
+            padding: EdgeInsets.all(size.width * 0.05),
+
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/ui/KertasGede.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+
+            child: Stack(
+              children: [
+                Center(
+                  child: Text(
+                    widget.textContent,
+                    style: TextStyle(
+                      fontSize: size.width * 0.05,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontFamily: 'BaskervvilleSC',
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: WoodButton(
+                    text: widget.buttonText,
+                    onTap: widget.onTap,
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+    );
+  }
+}
